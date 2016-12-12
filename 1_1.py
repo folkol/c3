ec73daec5f2290e01901455ae2d3342fe8e1c99b
Matte <mattias4@kth.se>
2016-12-12 12:40:25 +0100

    Moved code out of venv... doh!

---
+++ 1_1.py
@@ -0,0 +1,15 @@
+"""
+hex -> base64 encoding
+
+References:
+    https://en.wikipedia.org/wiki/Hexadecimal
+    https://en.wikipedia.org/wiki/Base64
+"""
+from funcs import hex_to_base64, assert_equals
+
+if __name__ == '__main__':
+    message = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
+    encoded = hex_to_base64(message)
+    expected = 'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'
+
+    assert_equals(expected, encoded)
---
+++ funcs.py
@@ -0,0 +1,36 @@
+def chunkify(lst, n):
+    """Given a list of a number n, yield consecutive chunks of size n."""
+    for pos in range(0, len(lst), n):
+        yield lst[pos:pos + n]
+
+
+def hex_to_ints(hex_string):
+    """Converts the hex string to a list of ints by converting chunks of 2 chars to int."""
+    return [int(chars, 16) for chars in chunkify(hex_string, 2)]
+
+
+def ints_to_base64(ints):
+    """Converts a list of ints, interpreted as byte values, to base64.
+
+    No padding support, which means that len(ints) must be a multiple of 3."""
+
+    def to_binary_string(i):
+        """Encodes the given number i as a binary string and strips the leading '0b'."""
+        strip_0b = bin(i)[2:]
+        return strip_0b.zfill(8)
+
+    from string import ascii_uppercase, ascii_lowercase, digits
+    numerals = ascii_uppercase + ascii_lowercase + digits + '+/'
+
+    binary_string = ''.join(to_binary_string(x) for x in ints)
+    return ''.join([numerals[int(x, 2)] for x in chunkify(binary_string, 6)])
+
+
+def hex_to_base64(hex_string):
+    as_ints = hex_to_ints(hex_string)
+    as_base64 = (ints_to_base64(x) for x in chunkify(as_ints, 3))
+    return ''.join(as_base64)
+
+
+def assert_equals(expected, actual):
+    assert expected == actual, "Expected '{}', but got '{}'.".format(expected, actual)
